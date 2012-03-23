16867b3d328f59ad07697f9d8a1a384dbf9b0e2a
David Bengoa <david@bengoarocandio.com>
2012-03-23 17:29:45 +0100

    update

--- js/hackaton.js
+++ js/hackaton.js
@@ -4,17 +4,36 @@ function switchContext(ctx){
     $("#" + ctx).show();
 }
 
-function loadSong(){
-    switchContext("load");
 
-    setTimeout(function(){
-        switchContext("song");
-    },2000);
 
+function loadSong(songData){
+    $.each(["author", "title", "record"], function(i,tag){
+        $("#data-" + tag).text(songData[tag]);
+    });
+    $("#cover").attr("src", songData.cover);
+    $("#video").html('<iframe width="100" height="100" src="http://www.youtube.com/embed/' + songData.video + '?autoplay=1" frameborder="0" allowfullscreen></iframe>');
+
+    switchContext("song");
 
     return false;
 }
 
-$("#search-btn").click(loadSong);
-$("#main").submit(loadSong);
+function querySong(songData){
+    switchContext("load");
+
+    setTimeout(function(){
+        loadSong({
+            title: "Just Dance",
+            author: "Lady Gaga",
+            record: "The fame",
+            cover: "testcover.png",
+            video: "WBYkB_Rz7XM"
+        });
+    },200);
+
+    return false;
+}
+
+$("#search-btn").click(querySong);
+$("#main").submit(querySong);
 
